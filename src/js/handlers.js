89f44fae62e725c462d7b356f01d8d7588362c4a
ryekram <reymark.magsanay@gmail.com>
2026-04-08 19:19:02 +0800

    style: refactor color schemes and centered sketch area

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -4,7 +4,7 @@ export function handleHover(elements) {
   let cells = elements.querySelectorAll(".grid__cell");
   cells.forEach((cell) => {
     cell.addEventListener("mouseenter", (events) => {
-      cell.style.background = "#cff2bd";
+      cell.style.background = "#adadad";
     });
   });
 }
