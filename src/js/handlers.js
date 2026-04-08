530903130789fc0f34b00a9eb9b999d9e89b45ea
ryekram <reymark.magsanay@gmail.com>
2026-04-08 20:38:06 +0800

    refactor: replace prompt with popover input value and added validations

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -12,20 +12,49 @@ export function handleHover(elements) {
 export function elementsInit() {
     return {
         gridContainer: document.querySelector(".grid__container"),
-        promptButton: document.querySelector(".prompt__button")
+        promptButton: document.querySelector(".prompt__button"),
+        popOver: document.querySelector('#mypopover'),
+        inputSize: document.querySelector("#input__size"),
+        submitBtn: document.querySelector(".submit__btn"),
+        errorSpan: document.querySelector(".error__size-span")
     }
 }
 
 export function handleInit() {
-  let elements = elementsInit()
+  let elements = elementsInit();
   gridRender(undefined,elements.gridContainer)
   handleHover(elements.gridContainer)
   // gridInit(elements.gridContainer);
 }
 
+export function validateInput(size) {
+  let elements = elementsInit();
+  if (!size || isNaN(size)) {
+    elements.inputSize.style.border = "1px solid #ff7575";
+    elements.errorSpan.textContent = "Please input a valid number";
+    return false;
+  }
+  if(size >= 100 || size <= 0) {
+    elements.inputSize.style.border = "1px solid #ff7575" 
+    elements.errorSpan.textContent = "Please input value below 100 or greater than 0"
+    return false;
+  }
+  return true;
+}
+
+export function clearInputs() {
+  let elements = elementsInit();
+  elements.inputSize.style.border = "none"
+  elements.inputSize.value = "" 
+  elements.errorSpan.textContent = ""
+}
+
 export function handlePrompt() {
   let elements = elementsInit()
-  let size = prompt("Please enter size of grid: ")
+  let size = elements.inputSize.value;
+  let checker = validateInput(size);
+  if(!checker) return;
+  elements.popOver.hidePopover();
   gridRender(size, elements.gridContainer) 
   handleHover(elements.gridContainer)
 }
